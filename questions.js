f11a2e7de577528f5669e61f5696de2c647f8bdb
Milena Garcia <109882477+MilenaGarciaCosta@users.noreply.github.com>
2023-09-05 21:32:14 -0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,46 @@
+export default[
+    {
+        question: "Nunca te ________ de viajar en avión.",
+        answers: [
+            {option: "he gustado", correct: false},
+            {option: "has gustado", correct: true},
+            {option: "ha gustado", correct: false},
+            {option: "hemos gustado", correct: false},
+            {option: "habeis gustado", correct: false},
+            {option: "han gustado", correct: false}
+        ],
+    },
+    {
+        question: "Hoy ________ mucho y ha sido muy provechoso.",
+        answers: [
+            {option: "he estudiado", correct: true},
+            {option: "has estudiado", correct: false},
+            {option: "ha estudiado", correct: false},
+            {option: "hemos estudiado", correct: false},
+            {option: "habeis estudiado", correct: false},
+            {option: "han estudiado", correct: false},
+        ],
+    },
+    {
+        question: "Vosotros ________ el éxame de matematica.",
+        answers: [
+            {option: "he hecho", correct: false},
+            {option: "has hecho", correct: false},
+            {option: "ha hecho", correct: false},
+            {option: "hemos hecho", correct: false},
+            {option: "habeis hecho", correct: true},
+            {option: "han hecho", correct: false},
+        ],
+    },
+    {
+        question: "Ayer ________ un té y dormí como un bebé.",
+        answers: [
+            {option: "he tomado", correct: true},
+            {option: "has tomado", correct: false},
+            {option: "ha tomado", correct: false},
+            {option: "hemos tomado", correct: false},
+            {option: "habeis tomado", correct: false},
+            {option: "han tomado", correct: false},
+        ],
+    },
+];
